abb38b2a1a5fb34b3130857cb908771329afd78c
fortytwode <shamanth@rocketshiphq.com>
2025-01-01 19:54:35 +0100

    Initial Commit

---
+++ Yoga Nidra/Yoga_NidraApp.swift
@@ -0,0 +1,17 @@
+//
+//  Yoga_NidraApp.swift
+//  Yoga Nidra
+//
+//  Created by Shamanth Rao on 1/1/25.
+//
+
+import SwiftUI
+
+@main
+struct Yoga_NidraApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
